43ed589625163d5c29bf03bc7b79063541008fca
tommyming <tommyhonym@gmail.com>
2021-06-02 00:20:51 +0800

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Results />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Results
   }
 }
 </script>
---
+++ src/components/Button-Go.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Button-Go"
+}
+</script>
+
+<style scoped>
+
+</style>
